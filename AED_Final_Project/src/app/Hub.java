8963942e08b2e9a14b78e39a054d23b0179f11df
rkarwankar <karwankar.r@northeastern.edu>
2022-12-06 00:21:43 -0500

    Hub Class Creation

---
+++ AED_Final_Project/src/app/Hub.java
@@ -0,0 +1,10 @@
+package app;
+public class Hub {
+    private Enterprise country;
+    private int id;
+
+    public Hub(Enterprise country, int id){
+        this.country = country;
+        this.id = id;
+    }
+}
