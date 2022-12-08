63a9e853c04c2ead0c3c140dd6701a1b35a90882
rkarwankar <karwankar.r@northeastern.edu>
2022-12-08 02:21:55 -0500

    LogManager Update

--- AED_Final_Project/src/app/LogManager.java
+++ AED_Final_Project/src/app/LogManager.java
@@ -1,16 +1,16 @@
 package app;
-
-
-import app.FrameWindow;
-
 public class LogManager extends Manager{
 
     private static String message;
 
+    public LogManager(String username, String password) {
+        super(username, password);
+    }
+
     public static void setMessage(String message) {LogManager.message = message;
     }
 
-    public static void displayMessage(){FrameWindow.setText(message);}
+    public static void displayMessage(){JFrameWindow.setText(message);}
 
     @Override
     void manage() {
