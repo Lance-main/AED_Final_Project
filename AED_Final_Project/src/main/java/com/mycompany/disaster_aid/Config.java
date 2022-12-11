f4683ad8c23edee26408c27f0fefd7f49a91b918
rkarwankar <karwankar.r@northeastern.edu>
2022-12-11 04:23:19 -0500

    updating config

---
+++ AED_Final_Project/src/main/java/com/mycompany/disaster_aid/Config.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.disaster_aid;
+import java.awt.Color;
+/**
+ *
+ * @author EpiCon
+ */
+public class Config {
+    public static final int Width = 1100;
+    public static final int Height = 720;
+    public static final int NoDbConnection =0;
+    public static final int LoginState =1;
+    public static final int RegisterState = 2;
+    public static final int Home_Scene_User = 3;
+    public static final int Home_Scene_Hub = 4;
+    public static final int Home_Scene_DeliverAgent = 5;
+    public static final int Home_Scene_EnterPrise = 6;
+    public static final int Home_Scene_Organization = 7;
+    public static final int Home_Scene_Logger = 8;
+    public static final int Home_Scene_Admin = 9;
+
+    public static final Color bgColor = new Color(0x30144B);
+    
+}
