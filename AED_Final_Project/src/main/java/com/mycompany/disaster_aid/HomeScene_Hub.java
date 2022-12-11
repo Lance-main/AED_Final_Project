77b79b2a3c1abee5efe8481ae0095b49e74158d7
Lance Main <lancemain7@gmail.com>
2022-12-11 04:35:17 -0500

    Homescene_hub.java updation

---
+++ AED_Final_Project/src/main/java/com/mycompany/disaster_aid/HomeScene_Hub.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.disaster_aid;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author 
+ */
+public class HomeScene_Hub extends JPanel implements Scenes{
+    
+    private AppWindow root;
+    HomeScene_Hub(AppWindow root)
+    {
+          this.root=root;
+          ArrayList<Component> ComponentList = new ArrayList();
+          ComponentList.add(addButton("Send Order",300,60,new Color(0xcfa12)));
+          ComponentList.add(addButton("Track Order",300,60,new Color(0xcfa12)));
+          ComponentList.add(addButton("Log Out",300,60,new Color(0xcfa12)));
+
+          this.setBackground(Config.bgColor);
+          this.add(addjpanel(new Color(0xaaca),FlowLayout.CENTER,50,50,650,650,ComponentList));
+    }
+
+    @Override
+    public void switchScene(int a)
+    {
+         root.appManager.SwitchState(a);
+    }
+    
+    @Override
+    public void init()
+    {
+         root.setContentPane(this);
+         update(root);
+    }
+}
