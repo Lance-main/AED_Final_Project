10eef0c3807b7886cdde5d851d26b1279d1ece36
Lance Main <lancemain7@gmail.com>
2022-12-10 22:03:42 -0500

    Disaster_aid.java class update

---
+++ AED_Final_Project/src/main/java/com/mycompany/disaster_aid/Disaster_aid.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.disaster_aid;
+
+/**
+ *
+ * @lance
+ */
+public class Disaster_aid {
+
+    public static void main(String[] args) {
+        new AppWindow("Disaster Aid",900,720);
+    }
+}
